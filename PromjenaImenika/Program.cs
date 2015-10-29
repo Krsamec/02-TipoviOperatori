cb45d7a0db3334a523b69cc9fdad34968db4a252
Krsamec <krsamec@vsite.hr>
2015-10-29 20:54:25 +0100

    multiple class

--- PromjenaImenika/Program.cs
+++ PromjenaImenika/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace PrviImenik
+﻿
+//using DrugiImenik
+namespace PrviImenik
 {
     class Program
     {
@@ -6,8 +8,12 @@
         {
             MojaKlasa mk1 = new MojaKlasa();
             mk1.Metoda1();
-            MojaKlasa mk2 = new MojaKlasa();
+            //MojaKlasa mk2 = new MojaKlasa();
+            DrugiImenik.MojaKlasa mk2 = new DrugiImenik.MojaKlasa();
             mk2.Metoda2();
+
+            // dva pristupa, da se navede deklaracija i ime imenika ili navesti puni naziv klase
+            //
         }
     }
 }
